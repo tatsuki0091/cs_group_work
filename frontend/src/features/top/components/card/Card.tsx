2de404ab6a66059add811c216f194fbcb22646a6
Tatsuki Yamatani <tatsuki091@gmail.com>
2024-06-06 02:10:42 -0700

    Created hooks

--- frontend/src/features/top/components/card/Card.tsx
+++ frontend/src/features/top/components/card/Card.tsx
@@ -2,61 +2,27 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './styles.module.css';
-import { Carousel, CarouselItem } from '@material-tailwind/react';
-type prevArrow = (args: {
-    loop: boolean;
-    handlePrev: () => void;
-    activeIndex: number;
-    firstIndex: boolean;
-}) => React.ReactNode | void;
+import { TESTIMONIES } from './constants'
+import { Testimonies } from "./interfaces"
 
-type nextArrow = (args: {
-    loop: boolean;
-    handleNext: () => void;
-    activeIndex: number;
-    lastIndex: boolean;
-}) => React.ReactNode | void;
-
-type navigation = (args: {
-    setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
-    activeIndex: number;
-    length: number;
-}) => React.ReactNode | void;
 
 const Card = () => {
+    const testimonies: Testimonies[] = TESTIMONIES
     return (
         <>
             <section className={`${styles.bg1}`}>
                 <h2 className={`${styles.c}`}><span className={`${styles.fadeInText}`}>Information</span><span className={`${styles.hosoku}`}>ご案内</span></h2>
-
-
                 <div className={`${styles.listGrid}`}>
-                    <div className={`${styles.list}`}>
-                        {/* <figure><Image src="images/internet.jpg" alt="" /></figure> */}
-                        <div className="text">
-                            <h4>ここにタイトルを入れます</h4>
-                            <p>ここに説明を入れます。サンプルテキスト。</p>
+                    {testimonies.map((testimony, index) => (
+                        <div key={index} className={`${styles.list}`}>
+                            {/* <figure><Image src="images/internet.jpg" alt="" /></figure> */}
+                            <div className="text">
+                                <h4>{testimony.title}</h4>
+                                <p>{testimony.paragraph}</p>
+                            </div>
+                            <p className={`${styles.btnBorderRadius}`}><Link href="service2.html">詳しくみる</Link></p>
                         </div>
-                        <p className={`${styles.btnBorderRadius}`}><Link href="service2.html">詳しくみる</Link></p>
-                    </div>
-
-                    <div className={`${styles.list}`}>
-                        {/* <figure><Image src="images/internet.jpg" alt="" /></figure> */}
-                        <div className="text">
-                            <h4>ここにタイトルを入れます</h4>
-                            <p>ここに説明を入れます。サンプルテキスト。</p>
-                        </div>
-                        <p className={`${styles.btnBorderRadius}`}><Link href="service2.html">詳しくみる</Link></p>
-                    </div>
-
-                    <div className={`${styles.list}`}>
-                        {/* <figure><Image src="images/internet.jpg" alt="" /></figure> */}
-                        <div className="text">
-                            <h4>ここにタイトルを入れます</h4>
-                            <p>ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。</p>
-                        </div>
-                        <p className={`${styles.btnBorderRadius}`}><Link href="service2.html">詳しくみる</Link></p>
-                    </div>
+                    ))}
 
                 </div>
 
---
+++ frontend/src/features/top/components/card/constants.ts
@@ -0,0 +1,12 @@
+import { Testimonies } from "./interfaces"
+export const TESTIMONIES: Testimonies[] = [
+    {
+        title: 'title',
+        paragraph: 'ここに説明を入れます。サンプルテキスト。'
+    },
+    {
+        title: 'title',
+        paragraph: 'ここに説明を入れます。サンプルテキスト。'
+    },
+    { title: 'title', paragraph: 'ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。' },
+]; 
