7e24d04b97bee2b8ce5ebf20b2518ee10386a85b
Tatsuki Yamatani <tatsuki091@gmail.com>
2024-06-07 02:10:58 -0700

    Finsihed creating hooks

---
+++ frontend/src/hooks/useForm.ts
@@ -0,0 +1,24 @@
+import { axiosMethods } from "../helpers/axios/axiosMap";
+
+interface formProps<T> {
+    values: T;
+    url: string;
+    httpMethod: string;
+    // onSubmit: (values: T) => void;
+}
+
+export const useForm = async <T extends object>({
+    values,
+    url,
+    httpMethod,
+}: formProps<T>) => {
+    const response = await axiosMethods[httpMethod](url, values)
+        .then((data) => {
+            return data;
+        })
+        .catch((err) => {
+            return err;
+        });
+
+    return response;
+};
