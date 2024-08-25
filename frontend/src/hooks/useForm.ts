1911d72decbf4f295cb749252de6fd50222b9d57
Tatsuki Yamatani <tatsuki091@gmail.com>
2024-08-24 20:09:22 -0700

    Updated axios custom hooks

--- frontend/src/hooks/useForm.ts
+++ frontend/src/hooks/useForm.ts
@@ -4,22 +4,43 @@ interface formProps<T> {
     values: T;
     url: string;
     httpMethod: string;
+    headers?: object
     // onSubmit: (values: T) => void;
 }
 
-export const useForm = async <T extends object>({
+
+export const useForm = async<T extends object>({
     values,
     url,
     httpMethod,
+    headers = {
+        "Content-Type": "application/json",
+    }
 }: formProps<T>) => {
-    const response = await axiosMethods[httpMethod](url, values)
-        .then((data) => {
-            return data;
-        })
-        .catch((err) => {
-            console.log(err)
-            return err;
-        });
+    let response = null
+    if (httpMethod === 'post' || httpMethod === 'patch') {
+        console.log(headers)
+        response = await axiosMethods[httpMethod](url, values, { headers })
+            .then((data) => {
+                return data;
+            })
+            .catch((err) => {
+                console.log(err)
+                return err;
+            });
+
+    } else if (httpMethod === 'get') {
+        response = await axiosMethods[httpMethod](url, { values, headers },)
+            .then((data) => {
+                return data;
+            })
+            .catch((err) => {
+                console.log(err)
+                return err;
+            });
+
+    }
+
 
     return response;
 };
