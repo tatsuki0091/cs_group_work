4143a58680a528f407cd64a1391a32608e530d8d
Tatsuki Yamatani <tatsuki091@gmail.com>
2024-08-24 18:14:23 -0700

    modified login component in backend to return object as an error response

--- backend/users/views/login_view.py
+++ backend/users/views/login_view.py
@@ -9,8 +9,9 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 # from rest_framework.views import APIView
 from django.contrib.auth import get_user_model, login, logout
-from users.validations import custom_validation, validate_email, validate_password
+from users.validations import validate_email, validate_password
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.core.exceptions import ValidationError
 
 
 class LoginUser(generics.GenericAPIView):
@@ -19,15 +20,19 @@ class LoginUser(generics.GenericAPIView):
 
     def post(self, request):
         data = request.data
-        assert validate_email(data)
-        assert validate_password(data)
-        serializer = LoginUserSerializer(data=data)
-        print(serializer.is_valid())
-        if serializer.is_valid():
-            user = serializer.check_user(data)
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            }, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            validate_email(data)
+            validate_password(data)
+            serializer = LoginUserSerializer(data=data)
+            if serializer.is_valid():
+                user = serializer.check_user(data)
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                }, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except (ValidationError, Exception) as e:
+            print(f"Caught an exception: {e}")
+            return Response({'error': e}, status=400)
