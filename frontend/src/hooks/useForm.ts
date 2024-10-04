93aace29c873e2368069830ac2561ac7b7785851
Tatsuki Yamatani <tatsuki091@gmail.com>
2024-10-03 23:55:52 -0700

    Editted axios configuration in the frontend

--- frontend/src/hooks/useForm.ts
+++ frontend/src/hooks/useForm.ts
@@ -1,46 +1,42 @@
-import { axiosMethods } from "../helpers/axios/axiosMap";
-
+import { axiosMethods } from '../helpers/axios/axiosMap';
 interface formProps<T> {
     values: T;
     url: string;
     httpMethod: string;
-    headers?: object
-    // onSubmit: (values: T) => void;
+    headers?: object;
 }
 
-
-export const useForm = async<T extends object>({
+export const useForm = async <T extends object>({
     values,
     url,
     httpMethod,
     headers = {
-        "Content-Type": "application/json",
-    }
+        'Content-Type': 'application/json',
+    },
 }: formProps<T>) => {
-    let response = null
+    let response = null;
     if (httpMethod === 'post' || httpMethod === 'patch') {
-        console.log(headers)
-        response = await axiosMethods[httpMethod](url, values, { headers })
+        const response = await axiosMethods[httpMethod](url, values, {
+            headers,
+        })
             .then((data) => {
                 return data;
             })
             .catch((err) => {
-                console.log(err)
+                console.log(err);
                 return err;
             });
-
+        return response;
     } else if (httpMethod === 'get') {
-        response = await axiosMethods[httpMethod](url, { values, headers },)
+        response = await axiosMethods[httpMethod](url, { values, headers })
             .then((data) => {
                 return data;
             })
             .catch((err) => {
-                console.log(err)
+                console.log(err);
                 return err;
             });
-
     }
-
 
     return response;
 };
