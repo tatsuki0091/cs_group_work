00117a5f8f5102ca8ff396a643892bafd41f3728
Tatsuki Yamatani <tatsuki091@gmail.com>
2024-10-09 18:35:40 -0700

    Editted logout function

--- backend/users/views/logout_view.py
+++ backend/users/views/logout_view.py
@@ -1,5 +1,17 @@
 from rest_framework import generics
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from django.contrib.auth import logout
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class Logout(generics.GenericAPIView):
-    print('logout')
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        logout(request)
+        response = Response(status=status.HTTP_200_OK)
+
+        return response
